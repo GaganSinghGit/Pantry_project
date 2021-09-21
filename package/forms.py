7a0993770bd839bc1a4d08b9fb631e13ac531a99
Gagan <81000542+GaganSinghGit@users.noreply.github.com>
2021-09-21 16:01:12 +1000

    aasfasd

---
+++ package/forms.py
@@ -0,0 +1,19 @@
+from typing import ItemsView
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, SelectField
+from wtforms.validators import Length, Email, DataRequired
+#from package.models import User
+
+# class LoginForm(FlaskForm):
+#     username = StringField(label='User Name', validators=[DataRequired()])
+#     password = PasswordField(label='Password', validators=[DataRequired()])
+#     submit = SubmitField(label='Sign in')
+
+class RequestForm(FlaskForm):
+    my_choices = ["apples", "cheese", "nuclear weapons"]
+    item = SelectField(label='Item:',choices=my_choices, validators=[DataRequired()])
+    email_address = StringField(label='Student Email:', validators=[Email(), DataRequired()])
+    student_id = StringField(label='Student Id:', validators=[Length(min=2,max=30), DataRequired()])
+
+    submit = SubmitField(label='Submit form')
+    
---
+++ package/models.py
@@ -0,0 +1,4 @@
+from package import db, login_manager
+from package import bcrypt
+from flask_login import UserMixin
+
